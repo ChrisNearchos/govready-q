746072ead2966f8d02ef3791aea8ddbe392c8442
Joshua Tauberer <jt@occams.info>
2018-01-31 16:51:58 -0500

    add a new 'text' output template format that skips markdown processing
    I used it to test the new lifecycle stage document.

--- guidedmodules/module_logic.py
+++ guidedmodules/module_logic.py
@@ -931,6 +931,7 @@ class ModuleAnswers(object):
         # module's output. The output is a set of documents. The
         # documents are lazy-rendered because not all of them may
         # be used by the caller.
+        output_formats = ("html", "text")
         class LazyRenderedDocument:
             def __init__(self, module_answers, document, index, use_data_urls):
                 self.module_answers = module_answers
@@ -940,11 +941,14 @@ class ModuleAnswers(object):
                 self.use_data_urls = use_data_urls
             def __iter__(self):
                 for key, value in self.document.items():
-                    if key != "html":
+                    if key not in output_formats:
                         yield key
-                    yield "html"
+                for key in output_formats:
+                    yield key
             def __getitem__(self, key):
-                if key == "html":
+                if key in output_formats:
+                    # key is an output format -> lazy render.
+
                     if self.rendered_content is None:
                         # For errors, what is the name of this document?
                         if "id" in self.document:
@@ -957,12 +961,14 @@ class ModuleAnswers(object):
 
                         # Try to render it.
                         try:
-                            self.rendered_content = render_content(self.document, self.module_answers, "html", doc_name, additional_context, show_answer_metadata=True, use_data_urls=use_data_urls)
+                            self.rendered_content = render_content(self.document, self.module_answers, key, doc_name, additional_context, show_answer_metadata=True, use_data_urls=use_data_urls)
                         except Exception as e:
                             # Put errors into the output. Errors should not occur if the
                             # template is designed correctly.
-                            import html
-                            self.rendered_content = "<p class=text-danger>" + html.escape(str(e)) + "</p>"
+                            self.rendered_content = str(e)
+                            if key == "html":
+                                import html
+                                self.rendered_content = "<p class=text-danger>" + html.escape(self.rendered_content) + "</p>"
 
                     return self.rendered_content
                 elif key in self.document:
